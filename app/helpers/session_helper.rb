d29157e5a5e5dea807c3fcbd1a9b62707c1e2d06
Elias <elias.wolzogen@yandex.ru>
2018-04-01 21:43:12 +0300

    Вспомогательные методы определения аутентификации в системе в SessionHelper

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -4,7 +4,17 @@ module SessionHelper
     @current_user ||= User.find session[:user_id]
   end
 
-  def user_authenticated?
-    current_user
+  def authenticated_user?; current_user; end
+
+  def require_authentication
+    unless authenticated_user?
+      redirect_to root_path, notice: 'Please sign in or register'
+    end
+  end
+
+  def not_require_authentication
+    if authenticated_user?
+      redirect_to root_path, notice: 'Please sign out on your account'
+    end
   end
 end
