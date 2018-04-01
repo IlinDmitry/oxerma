ce12bac2b75fb8816fc08f8ade9a0bc72c3c6133
Elias <elias.wolzogen@yandex.ru>
2018-04-01 21:51:51 +0300

    Метод set_user вызывается только тогда, когда фильтр before_action :require_authentication не прерывает цепочку вызова

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,11 +51,10 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    is_current = authenticated_user? && current_user.id.eql?(params[:id].to_i)
+    is_current = current_user.id.eql? params[:id].to_i
     @user = is_current ? current_user : User.find(params[:id])
   rescue ActiveRecord::RecordNotFound => e
-    redirect_to authenticated_user? ? users_path : root_path,
-                notice: e.message
+    redirect_to users_path, notice: e.message
   end
   private :set_user
 
