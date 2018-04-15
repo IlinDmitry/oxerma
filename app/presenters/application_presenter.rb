a9a89ac5ee777f8ef096f67c41176f9b31e5a80d
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-04-15 17:25:33 +0300

    Добавление общих методов получения изображения моделей в ApplicationPresenter

--- app/presenters/application_presenter.rb
+++ app/presenters/application_presenter.rb
@@ -5,7 +5,22 @@ class ApplicationPresenter < SimpleDelegator
     model.created_at.strftime format
   end
 
+  def image_thumb_path
+    model.image.thumb.url || default_image_path(self.class)
+  end
+
+  def image_small_thumb_path
+    model.image.small_thumb.url || default_image_path(self.class)
+  end
+
   def model
     __getobj__
+  end
+
+  private
+
+  def default_image_path(context)
+    const = Object.const_get context.to_s.sub!('Presenter', '')
+    const::DEFAULT_IMAGE
   end
 end
