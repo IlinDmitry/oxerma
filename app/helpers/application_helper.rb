0127cd7c3a6b55c4f6c272dc049a6e998bcd9e71
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-04-16 20:00:31 +0300

    Метод presenter имеет возможность оборачивать в Presenter и модель, и коллекцию

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-  def present(model)
-    "#{model.class}Presenter".constantize.new model
+  def present(object)
+    lambda = -> (e) {"#{e.class}Presenter".constantize.new e}
+    return lambda.call(object) unless block_given?
+    object.each do |model|
+      yield lambda.call model
+    end
   end
 
   def bootstrap_class_for(flash_type)
