5d9918cf3e7532f28aca9da4bdbac71a13775a23
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-04-20 09:23:12 +0300

    Удаление ненужных методов present_created_at, present_updated_at в ApplicationPresenter

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -2,10 +2,10 @@ module SessionHelper
   def current_user
     return unless session[:user_id]
     @current_user ||= begin
-        User.includes(:roles).find session[:user_id]
-      rescue ActiveRecord::RecordNotFound
-        nil
-      end
+      User.includes(:roles).find session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
   end
 
   def user_authenticated?; current_user; end
--- app/presenters/application_presenter.rb
+++ app/presenters/application_presenter.rb
@@ -1,14 +1,6 @@
 require 'delegate'
 
 class ApplicationPresenter < SimpleDelegator
-  def present_created_at(format = '%Y-%m-%d')
-    model.created_at.strftime format
-  end
-
-  def present_updated_at(format = '%Y-%m-%d')
-    model.updated_at.strftime format
-  end
-
   def model
     __getobj__
   end
