6530a91a55c702cb0403f05899253b4c2c310371
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-04-20 09:21:53 +0300

    Удаление DEFAULT_IMAGE из концерна Imagenable. Используем стандартный метод Carrierwave

--- app/models/concerns/imagenable.rb
+++ app/models/concerns/imagenable.rb
@@ -4,8 +4,6 @@ module Imagenable
   included do
     mount_uploader :image, ImageUploader
 
-    DEFAULT_IMAGE = 'default.png'
-
     validates :image,
               file_size: {less_than: 512.kilobytes}
   end
