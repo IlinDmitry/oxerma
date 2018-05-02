e49dca8e9b12697dc295efa5e1aed8aa478d2ff8
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-05-02 23:25:46 +0300

    Удаление модели типов организаций. Снжение обращений к Postgres

--- app/models/organization_type.rb
+++
@@ -1,9 +0,0 @@
-class OrganizationType < ApplicationRecord
-  has_one :organization
-
-  validates :name,
-            uniqueness: {
-                case_sensitive: false
-            },
-            presence: true
-end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,6 @@
 class Role < ApplicationRecord
   scopify
 
-  TYPE_UNDEFINED = 'undefined'
-  TYPE_EXTERNALS = {consumer: 'потребитель', producer: 'производитель'}
-
   has_and_belongs_to_many :users,
                           join_table: :users_roles
   belongs_to :resource,
