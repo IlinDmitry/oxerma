06b36c67fb5fdd07bdce7703a162bd592b18253b
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-05-19 16:03:10 +0300

    Рефакторинг фильтра before_action с Policy в SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-  before_action only: [:new, :create] {authorize :Session}
-  before_action only: [:destroy] {authorize :Session}
+  before_action {authorize :Session}
 
   def new
   end
